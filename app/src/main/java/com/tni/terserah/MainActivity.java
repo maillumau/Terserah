2db790408f4573ef16b13932141daee8fa633b21
maillumau <48422882+maillumau@users.noreply.github.com>
2019-10-24 09:45:38 +0700

    Update MainActivity.java

--- app/src/main/java/com/tni/terserah/MainActivity.java
+++ app/src/main/java/com/tni/terserah/MainActivity.java
@@ -19,12 +19,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         varTeks1 = findViewById(R.id.id_teks1);
-        varTeks1.setText("Hello Popi");
+        //varTeks1.setText("Hello Popi");
         varTeks1.setText("Hello Dwi");
-        varTeks1.setText("Hello Riksa");
-        setNamaAngga();
+        //varTeks1.setText("Hello Riksa");
+        //setNamaAngga();
         setNamaMadeyuukkkkk();
+       // setNamaBerdua();
+        String namaMaul = "Hello Maul";
+        setNamaMaul(namaMaul);
+        String namaMulyadi = "Hello Mulyadi";
+        setNamaMaul(namaMulyadi);
+
+        String namaFilham = Terserah.TAG_FILHAM;
+        setNamaMaul(namaFilham);
+
+        Terserah rh = new Terserah();
+        String namaRifqi = rh.TAG_NAMA_RIFQI;
+        setNamaMaul(namaRifqi);
     }
+    public void setNamaBerdua(){
+        varTeks1.setText("Hello Eko");
+        varTeks1.setText("Hello Libri");
+    }
+    void setNamaMaul(String nama){
+        varTeks1.setText(nama);
+
+    }
+
 
 
 
