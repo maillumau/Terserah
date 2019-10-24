1edf74bafa4e17250f862c883b155576fe91246d
maillumau <48422882+maillumau@users.noreply.github.com>
2019-10-24 09:46:30 +0700

    Update Terserah.java

--- app/src/main/java/com/tni/terserah/Terserah.java
+++ app/src/main/java/com/tni/terserah/Terserah.java
@@ -1,4 +1,6 @@
 package com.tni.terserah;
 
-public class Konfigurasi {
+public class Terserah {
+    public static String TAG_FILHAM = "Hello Filham";
+    public String TAG_NAMA_RIFQI = "Hello Rifqi";
 }
